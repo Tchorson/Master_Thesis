bd4d60cf21821e4597b362ea2e63a6b02ca50bb0
Tchorson <tchorek.mateusz@gmail.com>
2020-08-02 02:25:57 +0200

    @MappedSuperClass to inherit properties, associations, and methods.

--- src/main/java/com/tchorek/routes_collector/database/model/BaseTrack.java
+++ src/main/java/com/tchorek/routes_collector/database/model/BaseTrack.java
@@ -6,9 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.Instant;
 
 @Getter
@@ -16,6 +14,7 @@ import java.time.Instant;
 @RequiredArgsConstructor
 @AllArgsConstructor
 @NoArgsConstructor
+@MappedSuperclass
 public abstract class BaseTrack {
 
     @Id
